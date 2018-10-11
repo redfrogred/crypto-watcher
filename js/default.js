61c2fc6b32db83dcc85f05940152812e2054b5cd
Matt <matt.garvin.416@gmail.com>
2018-10-11 13:32:09 -0400

    added messages, left off before 1st ajax call

--- js/default.js
+++ js/default.js
@@ -1,5 +1,44 @@
 $(function() {
 	
-	console.log ('working');
+	var APP = {};
+			APP.name = 'Crypto Watcher';
+			APP.version = '0.01';
+		
+	// ----------------------------------------
+	
+	function init() {
+		console.log ( APP.name + ' version ' + APP.version + ' initializing...' );
+		$( 'h1' ).text( APP.name + ' ' + timestamp() );
+		document.title = APP.name + ' v' + APP.version;
+		fetchCoinList();
+		return;
+	}
+
+	// ----------------------------------------
+	
+	function timestamp() {	
+		return new Date().toLocaleString();
+	}
+
+	// ----------------------------------------
+	
+	function showMessage( mssg, mssgClass ) {
+		$( '#messageBox' ).addClass( mssgClass );
+		$( '#messageBox' ).html( mssg );
+		return;
+	}
+	
+	// ----------------------------------------
+	
+	function fetchCoinList() {
+		showMessage( 'Fetching coin list...', '' )
+		// ajax here
+		return;
+	}
+	
+	// ----------------------------------------
+
+	init();
+	
 	 
 });
